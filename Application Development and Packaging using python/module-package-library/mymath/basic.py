fa44615121f2b8c647bd4ac4d1ebd7c104567ef0
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2024-07-12 01:40:52 +0200

    Add files via upload

---
+++ Application Development and Packaging using python/module-package-library/mymath/basic.py
@@ -0,0 +1,31 @@
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
+
+def add(a, b):
+    """
+    This function returns the sum of given numbers
+    """
+    return a + b
+
+def area_of_rectangle(l,b):
+    """
+    compute the area of a rectangle
+    """
+    area = l*b
+    return area
+
+def area_of_circle(R):
+    """
+    compute the area of a rectangle
+    """
+    area = 3.14159 * R * R
+    return area    
